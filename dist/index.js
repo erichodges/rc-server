8a99af9f8f8eaca80fa70f68b13d28addf32a85d
Eric Hodges <ehodges2004@gmail.com>
2020-08-25 15:09:37 -0700

    Chore: initial setup with typescript watch

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const orm = await core_1.MikroORM.init();
+console.log("yo world sup?");
+//# sourceMappingURL=index.js.map
