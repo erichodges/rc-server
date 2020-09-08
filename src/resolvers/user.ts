54d17dd017c02625cc217e8d7fdb13764ec37001
Eric Hodges <ehodges2004@gmail.com>
2020-09-08 14:17:05 -0700

    Setup: Login added to resolvers/user.ts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,17 @@
 import argon2 from 'argon2';
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver
+} from 'type-graphql';
 import { User } from '../entities/User';
 import { MyContext } from '../types';
+
+//  InputTypes are used as arguments
 @InputType()
 class UsernamePasswordInput {
   @Field()
@@ -9,6 +19,25 @@ class UsernamePasswordInput {
   @Field()
   password: string;
 }
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+// Object types get returned
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
@@ -23,5 +52,37 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'That username does not exist'
+          }
+        ]
+      };
+    }
+    const valid = await argon2.verify(user?.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'incorrect password'
+          }
+        ]
+      };
+    }
+    return {
+      user
+    };
   }
 }
