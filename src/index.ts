f9f5231c5c176635ca10fdf6cdd41bddfb4632c4
Eric Hodges <ehodges2004@gmail.com>
2020-09-09 15:44:58 -0700

    Issue with req during session setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { MikroORM } from '@mikro-orm/core';
 // import {__prod__ } from './constants';
 import { ApolloServer } from 'apollo-server-express';
+import connectRedis from 'connect-redis';
 import express from 'express';
+import session from 'express-session';
+import redis from 'redis';
 import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
+import { __prod__ } from './constants';
 // import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
+import { MyContext } from './types';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -16,12 +21,35 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        // disableTTL: true,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: 'lax', // csrf setup - google this
+        secure: __prod__ // Cookie only works with https! careful
+      },
+      saveUninitialized: false,
+      secret: 'ToDo:setup-dotenv-etc',
+      resave: false
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
   });
 
   apolloServer.applyMiddleware({ app });
