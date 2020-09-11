956b6d62ff1d28f5b1c1d8356b455af551eaf500
Eric Hodges <ehodges2004@gmail.com>
2020-09-11 15:35:38 -0700

    Chore: Cleaned up resolvers/user.ts

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -6,6 +6,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver
 } from 'type-graphql';
 import { User } from '../entities/User';
@@ -28,19 +29,6 @@ class FieldError {
   message: string;
 }
 
-// @Resolver()
-// export class UserResolver {
-//   @Query(() => userInfo, { nullable: true })
-//   async me(@Ctx() { req, em }: MyContext) {
-//     // checking for logged in user:
-//     if (!req.session.userId) {
-//       return null;
-//     }
-//     const user = await em.findOne(User, { id: req.session.userId });
-//     return user;
-//   }
-// }
-
 // Object types get returned
 @ObjectType()
 class UserResponse {
@@ -53,10 +41,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // checking for logged in user:
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -78,6 +76,7 @@ export class UserResolver {
         ]
       };
     }
+
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
@@ -98,6 +97,12 @@ export class UserResolver {
         };
       }
     }
+
+    // store user id session
+    // this will set a cookie on the user
+    // to keep them logged in
+    req.session.userId = user.id;
+
     return { user };
   }
 
