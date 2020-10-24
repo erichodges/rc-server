8842a390f0cddcf1513fc04ac46279b85424a022
Eric Hodges <ehodges2004@gmail.com>
2020-10-24 13:56:10 -0700

    Setup: added Vote.ts

---
+++ src/entities/Vote.ts
@@ -0,0 +1,28 @@
+// import { ObjectType, Field } from "type-graphql";
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryColumn } from "typeorm";
+import { Post } from "./Post";
+import { User } from "./User";
+
+// m to n
+// many to many
+// user <-> posts
+// user -> join table <- posts
+// user -> updoot <- posts
+
+@Entity()
+export class Vote extends BaseEntity {
+  @Column({ type: "int" })
+  value: number;
+
+  @PrimaryColumn()
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.votes)
+  user: User;
+
+  @PrimaryColumn()
+  postId: number;
+
+  @ManyToOne(() => Post, (post) => post.votes)
+  post: Post;
+}
