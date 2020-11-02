3ecf7b24947242baa1c4c0688afa5058da559650
Eric Hodges <ehodges2004@gmail.com>
2020-11-02 15:21:58 -0800

    Setup: Added change vote to post.ts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -14,6 +14,7 @@ import {
 } from 'type-graphql';
 import { getConnection } from 'typeorm';
 import { Post } from '../entities/Post';
+import { Updoot } from "../entities/Updoot";
 import { isAuth } from '../middleware/isAuth';
 import { MyContext } from '../types';
 
@@ -51,19 +52,51 @@ export class PostResolver {
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
 
-    await getConnection().query(`
-      START TRANSACTION;
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-      insert into updoot ("userId", "postId", value)
-      values ('${userId}',${postId},${realValue});
+    // the user has voted on the post before
+    // and they are changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+    update updoot
+    set value = $1
+    where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
 
-      update post
-      set points = points + ${realValue}
-      where id = ${postId};
+        await tm.query(
+          `
+          update post
+          set points = points + $1
+          where id = $2
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // has never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+    insert into updoot ("userId", "postId", value)
+    values ($1, $2, $3)
+        `,
+          [userId, postId, realValue]
+        );
 
-      COMMIT;
-      `
-    );
+        await tm.query(
+          `
+    update post
+    set points = points + $1
+    where id = $2
+      `,
+          [realValue, postId]
+        );
+      });
+    }
     return true;
   }
 
